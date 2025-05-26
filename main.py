8602046bac5f1a0d9347f3332fa360e4c72c2820
NIKOLE-LACUNZA <lacunzanikole@gmail.com>
2025-05-26 16:38:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+import os
 import openai
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from loader import cargar_pdf
-from vector_store import crear_vectorstore, responder_pregunta
 from dotenv import load_dotenv
-import os
-from vector_store import crear_y_guardar_vectorstore
+from vector_store import responder_pregunta
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import OpenAIEmbeddings
 
-load_dotenv()  # En local, permite cargar variables del archivo .env
+load_dotenv()  # Solo tiene efecto localmente
 
 app = FastAPI()
 
-# CORS
+# Configurar CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -23,23 +21,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Configura la API Key al iniciar la aplicación
+# Variable global
+vectorstore = None
+
 @app.on_event("startup")
 def startup_event():
     global vectorstore
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    print("Clave OpenAI cargada:", "✅" if openai.api_key else "❌ (no encontrada)")
+    print("Clave OpenAI cargada:", "✅" if openai.api_key else "❌")
 
-    # Carga y vectoriza el documento
-    
-texto = cargar_pdf("documentos/DS009-2025-EF-Reglamento-ley-de-contrataciones.publicas.pdf")
-crear_y_guardar_vectorstore(texto, "vector_db")
-
-vectorstore = FAISS.load_local(
+    # Solo carga el vectorstore ya guardado (rápido)
+    vectorstore = FAISS.load_local(
         "vector_db",
         OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY")),
         allow_dangerous_deserialization=True
     )
+
 class Pregunta(BaseModel):
     mensaje: str
 
