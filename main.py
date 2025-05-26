1d5422ef1bb11772b0544555104a10d5f3d459f2
sora153 <eltrompeb1002@gmail.com>
2025-05-26 16:24:57 -0500

    piloto 1.3

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from loader import cargar_pdf
 from vector_store import crear_vectorstore, responder_pregunta
 from dotenv import load_dotenv
 import os
+from vector_store import crear_y_guardar_vectorstore
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
 
 load_dotenv()  # En local, permite cargar variables del archivo .env
 
@@ -23,14 +26,20 @@ app.add_middleware(
 # Configura la API Key al iniciar la aplicación
 @app.on_event("startup")
 def startup_event():
+    global vectorstore
     openai.api_key = os.getenv("OPENAI_API_KEY")
     print("Clave OpenAI cargada:", "✅" if openai.api_key else "❌ (no encontrada)")
 
     # Carga y vectoriza el documento
-    global vectorstore
-    texto = cargar_pdf("documentos/DS009-2025-EF-Reglamento-ley-de-contrataciones.publicas.pdf")
-    vectorstore = crear_vectorstore(texto)
+    
+texto = cargar_pdf("documentos/DS009-2025-EF-Reglamento-ley-de-contrataciones.publicas.pdf")
+crear_y_guardar_vectorstore(texto, "vector_db")
 
+vectorstore = FAISS.load_local(
+        "vector_db",
+        OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY")),
+        allow_dangerous_deserialization=True
+    )
 class Pregunta(BaseModel):
     mensaje: str
 
--- vector_store.py
+++ vector_store.py
@@ -3,6 +3,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+import os
 
 def crear_vectorstore(texto: str) -> FAISS:
     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
@@ -11,6 +12,12 @@ def crear_vectorstore(texto: str) -> FAISS:
     embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
     vectorstore = FAISS.from_texts(fragmentos, embedding=embeddings)
     return vectorstore
+def crear_y_guardar_vectorstore(texto: str, carpeta: str):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    fragmentos = splitter.split_text(texto)
+    embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
+    vectorstore = FAISS.from_texts(fragmentos, embedding=embeddings)
+    vectorstore.save_local(carpeta)
 
 def responder_pregunta(pregunta: str, vectorstore: FAISS) -> str:
     llm = ChatOpenAI(model_name="gpt-4", openai_api_key=os.getenv("OPENAI_API_KEY"))
